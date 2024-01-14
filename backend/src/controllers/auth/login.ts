d6e760d8d6bf47ce5a61821cc1810c390b10106c
HureraNadeem <muhammadhureran8@gmail.com>
2024-01-15 00:00:36 +0500

    backend initial setup done

---
+++ backend/src/controllers/auth/login.ts
@@ -0,0 +1,10 @@
+import { ICustomRequest } from "../../types/express";
+
+const loginController = async (req: ICustomRequest, res: Response, next: (arg0: any) => any) => {
+
+
+};
+
+export default loginController;
+
+
---
+++ backend/src/server.ts
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+import mongoose from 'mongoose';
+const app = require('./app');
+
+import establishDBConnection from './config/db';
+
+console.log(process.env.MONGODB_CONNECTION_STRING_URL);
+console.log(process.env.MONGODB_URL_USERNAME);
+console.log(process.env.MONGODB_URL_PASSWORD);
+const DB = process.env.MONGODB_CONNECTION_STRING_URL?.replace(
+	'<USERNAME>',
+	process.env.MONGODB_URL_USERNAME ?? ''
+)?.replace(
+	'<PASSWORD>',
+	process.env.MONGODB_URL_PASSWORD ?? ''
+);
+
+establishDBConnection(DB ?? '');
+
+const PORT = process.env.PORT_NUMBER_DEV;
+
+app.listen(PORT, () => {
+	console.log(`Server is running on http://localhost:${PORT}`);
+});
