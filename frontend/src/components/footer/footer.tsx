6a31ebdb17fe0b44356a900c1e8e14646edbe258
HureraNadeem <muhammadhureran8@gmail.com>
2024-01-19 20:15:46 +0500

    frontend: home page integrated with backend

--- frontend/src/components/footer/footer.tsx
+++ frontend/src/components/footer/footer.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom"
 function Footer() {
     return (
         <>
-                <footer className='text-center py-3 tertiary-text bg-black'>
+                <footer className='fixed bottom-0 w-full text-center py-3 text-sm bg-black max-h-fit'>
                     {"Developed by"}
                     <span className='tertiary-text underline' >
                         <Link to={"https://www.linkedin.com/in/iamhurera/"} target="_blank" rel="noopener noreferrer" className="underline mx-1">
