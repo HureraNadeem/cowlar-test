e048bf8a883d15d90f0e0003d0a674c23151ad15
HureraNadeem <muhammadhureran8@gmail.com>
2024-01-22 14:42:34 +0500

    frontend: added navbar and footer on login and signup pages

--- frontend/src/components/navbar/navbar.tsx
+++ frontend/src/components/navbar/navbar.tsx
@@ -14,6 +14,9 @@ function Navbar() {
                     <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                         <span className="self-center text-2xl font-semibold whitespace-nowrap text-primaryRed">MovieTube</span>
                     </Link>
+                    <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
+                        <span className="self-center text-lg font-semibold whitespace-nowrap text-primaryRed hover:underline">Home</span>
+                    </Link>
                     {
                         isLoggedIn ? (
                             <>
--- frontend/src/pages/login/login.tsx
+++ frontend/src/pages/login/login.tsx
@@ -3,16 +3,22 @@ import { Link, useNavigate } from 'react-router-dom';
 import BasicButton from '../../components/button/basic-button';
 import { useForm } from 'react-hook-form';
 import { loginUser } from '../../api/user';
-import { Toaster } from 'react-hot-toast';
+import toast, { Toaster } from 'react-hot-toast';
 import { UserContext } from '../../context';
+import RootLayout from '../../Layout/RootLayout';
+import React from 'react';
+import useAuthVerification from '../../hooks/useAuthentication';
+import Loading from '../../components/loading';
 
 function LogIn() {
     const [submitLoading, setSubmitLoading] = useState<boolean>(false);
 
-    const { register, handleSubmit, formState: { errors }} = useForm();
+    const { register, handleSubmit, formState: { errors } } = useForm();
     const navigate = useNavigate();
 
-    const { setIsLoggedIn, updateUser } = useContext(UserContext);
+    const { isLoggedIn, setIsLoggedIn, updateUser } = useContext(UserContext);
+
+    const { pageLoading } = useAuthVerification();
 
     const onSubmit = async (data: any) => {
         setSubmitLoading(true);
@@ -34,85 +40,108 @@ function LogIn() {
         setSubmitLoading(false);
     };
 
+    React.useEffect(() => {
+        if (isLoggedIn) {
+            toast('You are already logged in! Log out first!', {icon: "🧐"})
+            navigate('/');
+        }
+    }, [isLoggedIn])
+
+    if (pageLoading) {
+        return (
+            <>
+                <Toaster />
+                <RootLayout>
+                    <div className="w-screen h-screen flex items-center justify-center">
+                        <Loading />
+                        <p className="mx-2">Loading ...</p>
+                    </div>
+                </RootLayout>
+            </>
+        );
+    }
+
     return (
         <>
             <Toaster />
-            <div
-                className='custom-bg-gradient flex min-h-screen items-center justify-center px-4'
-                style={{
-                    backgroundImage:
-                        'linear-gradient(to top, rgba(0, 0, 0, 0.8) 0, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.8) 100%),url(https://assets.nflxext.com/ffe/siteui/vlv3/594f8025-139a-4a35-b58d-4ecf8fdc507c/c11340ec-4211-4482-a9a0-f0ccd4d9e940/PK-en-20240108-popsignuptwoweeks-perspective_alpha_website_small.jpg)',
-                }}
-            >
-                <div className='w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0'>
-                    <div className='space-y-4 p-6 sm:p-8 md:space-y-6'>
-                        <h1 data-testid="login-heading" className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl'>
-                            Log in
-                        </h1>
-                        <form
-                            className='space-y-4 md:space-y-6'
-                            onSubmit={handleSubmit(onSubmit)}
-                        >
-                            <div>
-                                <label
-                                    htmlFor='email'
-                                    className='mb-2 block text-sm font-medium text-gray-900 '
-                                >
-                                    Email
-                                </label>
-                                <input
-                                    type='email'
-                                    id='email'
-                                    className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-primary-600 focus:ring-primary-600 sm:text-sm'
-                                    placeholder='name@service.com'
-                                    {...register('email', { required: 'Email is required' })}
-                                />
-                                {errors.email && <span className="text-sm text-red-500">{errors.email.message?.toString()}</span>}
-                            </div>
-                            <div>
-                                <label
-                                    htmlFor='password'
-                                    className='mb-2 block text-sm font-medium text-gray-900 '
-                                >
-                                    Password
-                                </label>
-                                <input
-                                    type='password'
-                                    id='password'
-                                    placeholder='••••••••'
-                                    className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-primary-600 focus:ring-primary-600 sm:text-sm'
-                                    {...register('password', {
-                                        required: 'Password is required',
-                                    })}
-                                />
-                                {errors.password && <span className="text-sm text-red-500">{errors.password.message?.toString()}</span>}
+            <RootLayout>
+                <div
+                    className='custom-bg-gradient flex min-h-screen items-center justify-center px-4'
+                    style={{
+                        backgroundImage:
+                            'linear-gradient(to top, rgba(0, 0, 0, 0.8) 0, rgba(0, 0, 0, 0) 60%, rgba(0, 0, 0, 0.8) 100%),url(https://assets.nflxext.com/ffe/siteui/vlv3/594f8025-139a-4a35-b58d-4ecf8fdc507c/c11340ec-4211-4482-a9a0-f0ccd4d9e940/PK-en-20240108-popsignuptwoweeks-perspective_alpha_website_small.jpg)',
+                    }}
+                >
+                    <div className='w-full rounded-lg bg-white shadow sm:max-w-md md:mt-0 xl:p-0'>
+                        <div className='space-y-4 p-6 sm:p-8 md:space-y-6'>
+                            <h1 data-testid="login-heading" className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl'>
+                                Log in
+                            </h1>
+                            <form
+                                className='space-y-4 md:space-y-6'
+                                onSubmit={handleSubmit(onSubmit)}
+                            >
+                                <div>
+                                    <label
+                                        htmlFor='email'
+                                        className='mb-2 block text-sm font-medium text-gray-900 '
+                                    >
+                                        Email
+                                    </label>
+                                    <input
+                                        type='email'
+                                        id='email'
+                                        className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-primary-600 focus:ring-primary-600 sm:text-sm'
+                                        placeholder='name@service.com'
+                                        {...register('email', { required: 'Email is required' })}
+                                    />
+                                    {errors.email && <span className="text-sm text-red-500">{errors.email.message?.toString()}</span>}
+                                </div>
+                                <div>
+                                    <label
+                                        htmlFor='password'
+                                        className='mb-2 block text-sm font-medium text-gray-900 '
+                                    >
+                                        Password
+                                    </label>
+                                    <input
+                                        type='password'
+                                        id='password'
+                                        placeholder='••••••••'
+                                        className='block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 text-gray-900 focus:border-primary-600 focus:ring-primary-600 sm:text-sm'
+                                        {...register('password', {
+                                            required: 'Password is required',
+                                        })}
+                                    />
+                                    {errors.password && <span className="text-sm text-red-500">{errors.password.message?.toString()}</span>}
 
-                            </div>
+                                </div>
 
-                            <div className='text-sm text-red-500'></div>
+                                <div className='text-sm text-red-500'></div>
 
-                            <div className='flex items-center justify-center'>
-                                <BasicButton
-                                    isLoading={submitLoading}
-                                    text='Log in'
-                                    loadingText='Loggin in ...'
-                                    type='submit'
-                                />
-                            </div>
+                                <div className='flex items-center justify-center'>
+                                    <BasicButton
+                                        isLoading={submitLoading}
+                                        text='Log in'
+                                        loadingText='Loggin in ...'
+                                        type='submit'
+                                    />
+                                </div>
 
-                            <p className='text-sm font-light text-gray-500 '>
-                                Don't have an account?{' '}
-                                <Link
-                                    to='/signup'
-                                    className='font-medium text-primaryRed hover:underline opacity-[0.9]'
-                                >
-                                    Sign Up
-                                </Link>
-                            </p>
-                        </form>
+                                <p className='text-sm font-light text-gray-500 '>
+                                    Don't have an account?{' '}
+                                    <Link
+                                        to='/signup'
+                                        className='font-medium text-primaryRed hover:underline opacity-[0.9]'
+                                    >
+                                        Sign Up
+                                    </Link>
+                                </p>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
+            </RootLayout>
         </>
     );
 }
